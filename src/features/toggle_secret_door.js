fa7dc8ae9e4603a4d28d8d4f984fc7ad9d38b63b
p4535992 <tentimarco0@gmail.com>
2021-12-27 12:40:31 +0100

    beta 1.3.0

--- src/features/toggle_secret_door.js
+++ src/features/toggle_secret_door.js
@@ -1,20 +1,23 @@
-import {settingsKey} from "../settings.js"
-import {updateSynchronizedDoors} from "./synchronized_doors.js";
+import { settingsKey } from '../settings.js';
+import { updateSynchronizedDoors } from './synchronized_doors.js';
 
 // Toggles between normal and secret doors
 export function onDoorLeftClick(event) {
-	// We don't trust the event to be filled with the expected data for compatibilty with arms reach (which passes a broken event)
-	if (game.settings.get(settingsKey, "toggleSecretDoors") && event.data?.originalEvent?.ctrlKey && game.user.isGM) {
-		const types = CONST.WALL_DOOR_TYPES
-		const newtype = this.wall.data.door === types.DOOR ? types.SECRET : types.DOOR
-		const updateData = {door: newtype}
-		const synchronizationGroup = this.wall.data.flags.smartdoors?.synchronizationGroup
-		if (game.settings.get(settingsKey, "synchronizedDoors") && synchronizationGroup && this.wall.data.flags.smartdoors?.synchronizeSecretStatus)
-			updateSynchronizedDoors(updateData, synchronizationGroup)
-		else
-			this.wall.document.update(updateData)
+  // We don't trust the event to be filled with the expected data for compatibilty with arms reach (which passes a broken event)
+  if (game.settings.get(settingsKey, 'toggleSecretDoors') && event.data?.originalEvent?.ctrlKey && game.user.isGM) {
+    const types = CONST.WALL_DOOR_TYPES;
+    const newtype = this.wall.data.door === types.DOOR ? types.SECRET : types.DOOR;
+    const updateData = { door: newtype };
+    const synchronizationGroup = this.wall.data.flags.smartdoors?.synchronizationGroup;
+    if (
+      game.settings.get(settingsKey, 'synchronizedDoors') &&
+      synchronizationGroup &&
+      this.wall.data.flags.smartdoors?.synchronizeSecretStatus
+    )
+      updateSynchronizedDoors(updateData, synchronizationGroup);
+    else this.wall.document.update(updateData);
 
-		return true
-	}
-	return false
+    return true;
+  }
+  return false;
 }
